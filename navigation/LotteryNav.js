f8139431030ecdd0d16a8c5001215ea0dc623f61
khanhtran45 <tuankhanhtran45@gmail.com>
2019-12-20 14:24:10 +0700

    format eslint

--- navigation/LotteryNav.js
+++ navigation/LotteryNav.js
@@ -1,23 +1,38 @@
-import React, { Component } from 'react';
-import { View, Platform, StyleSheet, Text, Image, Dimensions, TouchableOpacity } from "react-native";
-
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 
 export default class LotteryNav extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-    };
-
-    toggleDrawer = () => {
-        this.props.navigationProps.toggleDrawer();
-    };
-    render() {
-        return (
-            <View
-                style={{ flexDirection: 'row', paddingLeft: 10, backgroundColor: 'black', borderBottomColor: 'gray', borderBottomWidth: 0.2, }}>
-                <Text style={{ flex: 8, textAlign: "center", fontSize: 30, color: 'white' }}>Lottery Prize </Text>
-
-            </View>
-        )
-    }
-} 
+  toggleDrawer = () => {
+    this.props.navigationProps.toggleDrawer();
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingLeft: 10,
+          backgroundColor: 'black',
+          borderBottomColor: 'gray',
+          borderBottomWidth: 0.2,
+        }}>
+        <Text
+          style={{flex: 8, textAlign: 'center', fontSize: 30, color: 'white'}}>
+          Lottery Prize{' '}
+        </Text>
+      </View>
+    );
+  }
+}
